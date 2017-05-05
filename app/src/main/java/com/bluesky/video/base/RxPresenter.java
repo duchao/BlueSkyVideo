a9b6268ded242a1a0d0619c8ea99f43ce7f921a7
duchao <duchao@rd.netease.com>
2017-05-05 21:14:03 +0800

    FIX:修改最低版本

--- app/src/main/java/com/bluesky/video/base/RxPresenter.java
+++ app/src/main/java/com/bluesky/video/base/RxPresenter.java
@@ -1,5 +1,8 @@
 package com.bluesky.video.base;
 
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
 /**
  * Created by duchao on 2017/5/5.
  */
@@ -7,9 +10,23 @@ package com.bluesky.video.base;
 public class RxPresenter<T extends BaseView> implements BasePresenter<T> {
 
     protected T mView;
+    protected CompositeDisposable mCompositeDisposable;
     @Override
     public void attachView(T view) {
 
+    }
+
+    protected void addSubscribe(Disposable subscription) {
+        if (mCompositeDisposable == null) {
+            mCompositeDisposable = new CompositeDisposable();
+        }
+        mCompositeDisposable.add(subscription);
+    }
+
+    protected void unSubscribe() {
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.dispose();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/bluesky/video/presenter/SplashPresenter.java
@@ -0,0 +1,8 @@
+package com.bluesky.video.presenter;
+
+/**
+ * Created by duchao on 2017/5/5.
+ */
+
+public class SplashPresenter {
+}
